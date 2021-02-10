dd428e898402e5d441dd432de2a2e6ba491b498a
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-02-10 22:25:55 +0200

    client (auth)

---
+++ client/pages/auth/signin.js
@@ -0,0 +1,51 @@
+import {useState} from 'react'
+import Router from 'next/router'
+import useRequest from '../../hooks/use-request'
+
+export default () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const {errors, doRequest} = useRequest({
+        url: '/api/users/signin',
+        method: 'post',
+        body: {
+            email, password
+        },
+        onSuccess: () => Router.push('/')
+    })
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+
+        await doRequest()     
+    }
+
+    return (
+    <form onSubmit={onSubmit} className="container">
+        <h1>Sign In</h1>
+        <div className="form-group">
+            <label>Email Address</label>
+            <input 
+                style={{width:'50%'}}
+                value={email} 
+                onChange={e => setEmail(e.target.value)} 
+                className="form-control"
+                aria-describedby="emailHelp" 
+                placeholder="Enter email"
+            />
+        </div>
+        <div className="form-group">
+            <label>Password</label>
+            <input 
+                style={{width:'50%'}}
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                type="password" 
+                className="form-control"
+                placeholder="Password"
+            />
+        </div>
+        {errors}
+        <button className="btn btn-primary">Sign In</button>
+    </form>)
+}
