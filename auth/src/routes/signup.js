401b6e32aff55c5ee7753185e376f4ec2d52b0be
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-05-09 00:21:45 +0300

    some updates

--- auth/src/routes/signup.js
+++ auth/src/routes/signup.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const { body } = require('express-validator')
-const {signup} = require('../controllers/signupController') 
+const { signup } = require('../controllers/signupController')
 
 const router = express.Router()
 
-router.post('/', 
-[
-    body('email').isEmail().withMessage('Email must be valid'),
-    body('password')
-    .trim()
-    .isLength({ min :4, max: 20 })
-    .withMessage('Password between 4 and 20 characters')
-],
-signup)
+router.post('/',
+    [
+        body('email').isEmail().withMessage('Email must be valid'),
+        body('password')
+            .trim()
+            .isLength({ min: 4, max: 20 })
+            .withMessage('Password between 4 and 20 characters')
+    ],
+    signup)
 
 router.get('/', (req, res) => {
     res.send('work')
