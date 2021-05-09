c6f66f175994aefbb387d1cf095be1657bb45c65
Mykola-DMT <50734364+Mykola-DMT@users.noreply.github.com>
2021-05-09 21:41:29 +0300

    using signUp controller

--- auth/src/routes/signup.js
+++ auth/src/routes/signup.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { body } = require('express-validator')
-//const { signup } = require('../controllers/signupController')
+const { signup } = require('../controllers/signupController')
 
 const router = express.Router()
 
@@ -12,44 +12,7 @@ router.post('/',
             .isLength({ min: 4, max: 20 })
             .withMessage('Password between 4 and 20 characters')
     ],
-    async (req, res) => {
-        const errors = validationResult(req)
-
-        if (!errors.isEmpty()) {
-            return res.status(400).send({
-                errors: errors.array().map(error => {
-                    return { message: error.msg, field: error.param }
-                })
-            })
-        }
-
-        const { email, password } = req.body
-
-        const existingUser = await User.findOne({ email })
-        if (existingUser) {
-            return res.status(400).send({
-                errors: [{ message: 'Email in use' }]
-            })
-        }
-
-        const hashed = await bcrypt.hash(password, 12)
-        const user = new User({
-            email, password: hashed
-        })
-        await user.save()
-
-        const userJwt = jwt.sign({
-            id: user.id,
-            email: user.email
-        }, process.env.jwt_key)
-
-        //Store on session object
-        req.session = {
-            jwt: userJwt
-        }
-
-        res.status(201).send(user)
-    })
+    signup)
 
 router.get('/', (req, res) => {
     res.send('work')
